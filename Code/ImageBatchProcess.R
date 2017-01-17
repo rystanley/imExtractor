3517da90a0b61a27744ed0e38f05c092ac587dcd
Homarus23 <rstanley@mun.ca>
2017-01-17 08:24:21 -0400

    updates to batch process

--- Code/ImageBatchProcess.R
+++ Code/ImageBatchProcess.R
@@ -1,13 +1,15 @@
+
 ImageBatchProcess=function(folder,month=NULL,year=NULL,code=NULL,pix=200,offset=0.01,sigma=3,crop=150){
   
   setwd(folder) # set working directory to folder
-  
+
   files <- (Sys.glob("*.tif")) # identify any 
   
   Start=Sys.time() # timing funciton 
   
   ImageMetaData <- lapply(files,FUN=ImageProcess,pix=pix,offset=offset,sigma=sigma,crop=crop)
-  
+
+  #Set up timing parameters
   Timelog <- Sys.time()-Start
   Timelog <- round(as.numeric(Timelog,units="mins"),2)
   
@@ -17,11 +19,12 @@ ImageBatchProcess=function(folder,month=NULL,year=NULL,code=NULL,pix=200,offset=
   if(is.null(year)){year="Not specified"}
   if(is.null(month)){month="Not specified"}
   if(is.null(code)){code="Not specified"}
-  
+  if(is.null(sex)){sex="Not specified"}
   
   imagedata <- data.frame(Year=rep(year,nrow(output)),
                              Month=rep(month,nrow(output)),
-                             Code=rep(code,nrow(output)),stringsAsFactors=F)
+                             Code=rep(code,nrow(output)),
+                             sex=rep(sex,nrow(output)),stringsAsFactors=F)
   
   output=cbind(imagedata,output)
   
